214dcfeefd4e87aa4c24b9a867758d926e88518e
Eyzcode <fritjof.cavallin@gmail.com>
2018-03-12 14:27:26 +0100

    Added nonfunctional code

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,4 +7,14 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
+	
+	public int sub(int a, int b)
+	{
+		return 0;
+	}
+	
+	public int mul(int a, int b)
+	{
+		return 0;
+	}
 }
