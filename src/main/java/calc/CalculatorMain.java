8e68dc430e65d90a41d2d817f2de6df6409021d6
Axenu <simonseregon@gmail.com>
2018-03-12 14:55:31 +0100

    Made div test fail

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -2,24 +2,24 @@ package calc;
 
 public class CalculatorMain {
 	public CalculatorMain(){
-		
+
 	}
 	public int add(int a, int b){
 		return a+b;
 	}
-	
+
 	public int sub(int a, int b)
 	{
 		return 0;
 	}
-	
+
 	public int mul(int a, int b)
 	{
 		return 0;
 	}
-	
+
 	public int div(int a, int b)
 	{
-		return 0;
+		return -1;
 	}
 }
